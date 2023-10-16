20cac6b070e5248883e1946ba35d7f13ab1128c0
DJ <djolaleye@gmail.com>
2023-10-15 19:24:47 -0500

    Structure

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require('path')
+const rootDir = path.join(__dirname, '..');
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.set('views', rootDir + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get("/", (req, res) => {
+    const data = {
+
+    };
+
+    res.render("index.html", data);
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Listening  on port ${port}`);
+})
+
+
+// Footer
+$(function(){
+    let year = new Date().getFullYear();
+    $('#copyright_year').append(year);
+
+});
